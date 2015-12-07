2c5f585f228b2f37213d37f71fbbe7b512eaca96
Carl Humphreys <carlh.crimson@gmail.com>
2015-12-07 17:28:02 +0000

    Raindrop splash on bottom of window

--- js/scripts.js
+++ js/scripts.js
@@ -6,17 +6,14 @@ var canvas = document.getElementById("rainCanvas");
 var ctx = canvas.getContext("2d");
 
 var particles = [];
-var bannerX = 0;
-var bannerY = 0;
-var bannerWidth = 0;
-var bannerHeight = 0;
+var splashes = [];
 
 var init = function() {
 	registerEvents();
 	setCanvasSize();
 	
 	// # particles, radius, speed, wind
-	createParticles(150, 5, 2, 1);
+	createParticles(200, 5, 10, 3);
 	
 	render();
 
@@ -26,6 +23,7 @@ var registerEvents = function() {
 	window.addEventListener("resize", function() {
 		resize();
 	});
+	
 };
 
 var resize = function() {
@@ -43,95 +41,55 @@ var setCanvasSize = function() {
 
 
 // 0 radians is 3 oclock position
-// Converts angle into radians
-var drawCircle = function(x,y,radius){
+
+var drawRainDrop = function(x,y,radius, alpha){
 	ctx.beginPath();
-	ctx.arc(x,y,radius,0,2*Math.PI);
-
+	ctx.arc(x,y,radius,0,Math.PI);	
 	
-	ctx.shadowBlur=20;
-	ctx.shadowColor="white";
-	ctx.fillStyle = "white";
+	ctx.fillStyle = "rgba(0,102,255," + alpha +")";
+	ctx.moveTo((x-radius), y)
+	ctx.lineTo(x,y-2*radius);
+	ctx.lineTo((x+radius),y);
+	// ctx.closePath();
+	ctx.fill();	
+
+};
+
+var drawSplash = function(x, y, endRadius) {
+	ctx.beginPath();
+	ctx.arc(x, y, endRadius, 0, 2*Math.PI);
 	ctx.fill();
-
 };
-
-var drawFlake = function(x,y,lineLength, n) {
-	
-	
-	var theta = (2*Math.PI)/n;
-		
-	ctx.strokeStyle = "white";
-	// ctx.shadowBlur=30;
-	ctx.shadowColor="white";
-
-	for (var i =0; i < n; i++) {
-
-		var dx = Math.round((lineLength/2) * Math.cos(theta*i));
-		var dy = Math.round((lineLength/2) * Math.sin(theta*i));
-			
-		ctx.beginPath();
-		ctx.moveTo(x, y);
-		ctx.lineTo((x + dx), (y + dy));
-		ctx.stroke();			
-	}
-		
-
-};
-
-
-var fillBoxAnim = function(boxWidth, boxHeight, percentage){
-	bannerX = (width/2) - (boxWidth/2);
-	bannerY = (height - 60);
-	bannerWidth = boxWidth;
-	bannerHeight = boxHeight;
-	ctx.shadowBlur=20;
-	ctx.shadowColor = "white";
-	ctx.fillStyle = "black";
-	
-	ctx.rect(bannerX, bannerY, bannerWidth, (percentage) * bannerHeight);
-	ctx.fill();
-
-};
-
-
-
-var percent = 0;
 
 
 var render = function() {
 	ctx.clearRect(0,0,width,height);
 	
 	requestAnimationFrame(render); 
+	
 
    
  	for (var i = 0; i < particles.length; i++) {
  		
+ 		drawRainDrop(particles[i].position.x += particles[i].speed.x , 
+			   	   particles[i].position.y += particles[i].speed.y, 
+ 				   particles[i].radius, particles[i].alpha);
+		if (particles[i].position.y >= height - (2*particles[i].radius)) {
+
+			drawSplash(particles[i].position.x, height - (2*particles[i].radius), splashes[i].endRadius);
+
  		
-		if (particles[i].position.y >= height + particles[i].radius) {
+ 		}
+		if (particles[i].position.y >= height - particles[i].radius) {
 
  			 particles[i].position.y = 0 - particles[i].radius;
- 			 if (percent < 100) {
- 			 	percent += 1;
- 			 }
-
+ 		
  		}
  		if (particles[i].position.x >= width + particles[i].radius) {
 
  			 particles[i].position.x = 0 - particles[i].radius;
- 			 if (percent < 100) {
- 			 	percent += 1;
- 			 }
-
- 		}
-			// drawFlake(particles[i].position.x += particles[i].speed.x , 
- 		// 		  	 particles[i].position.y += particles[i].speed.y, 
- 		// 		   	particles[i].radius, 6);
-	
-		
-		drawCircle(particles[i].position.x += particles[i].speed.x , 
-			   	   particles[i].position.y += particles[i].speed.y, 
- 				   particles[i].radius);
+ 			 
+ 		}	
 		
  	}
     
@@ -144,8 +102,14 @@ var Particle = function() {
 	this.position = {x: 0, y: 0};
 	this.speed = {x: 0, y: 0};
 
-}
+};
 
+var Splash = function() {	
+	
+	this.endRadius = 20;
+
+
+};
 var createParticles = function(numParticles, radius, speed, wind) {
 	
 	for (var i = 0; i < numParticles; i++) {
@@ -153,10 +117,15 @@ var createParticles = function(numParticles, radius, speed, wind) {
 		p.radius = (Math.random() * radius) + 1;
 		p.position.x = Math.random() * width;
 		p.position.y = -1 * Math.random()*height;
-		p.speed.y = 2 + (speed * Math.random());
+		p.speed.y = 5 + (speed * Math.random());
 		p.speed.x = (0.5 * wind) - (wind * Math.random());
-
+		p.alpha = Math.random();
 		particles.push(p);
+
+		var s = new Splash();
+		s.endRadius = p.radius+(p.speed.y/2);		
+		splashes.push(s);
+
 	}
 };
 init();
