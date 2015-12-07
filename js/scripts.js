e0e8facf8e6913e668c0e68b1246097d5d715d20
Carl Humphreys <carlh.crimson@gmail.com>
2015-12-07 15:33:53 +0000

    Using completed snowfall as a basis

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,23 @@
 'use strict'
-
-var width = window.innerWidth;
-var height = window.innerHeight;
-
+var container = document.getElementById("container")
+var width = container.offsetWidth;
+var height = container.offsetHeight;
 var canvas = document.getElementById("rainCanvas");
-
 var ctx = canvas.getContext("2d");
 
 var particles = [];
+var bannerX = 0;
+var bannerY = 0;
+var bannerWidth = 0;
+var bannerHeight = 0;
 
 var init = function() {
 	registerEvents();
 	setCanvasSize();
-
-	createParticles(40);
-
+	
+	// # particles, radius, speed, wind
+	createParticles(150, 5, 2, 1);
+	
 	render();
 
 };
@@ -30,8 +33,8 @@ var resize = function() {
 };
 
 var setCanvasSize = function() {
-	width = window.innerWidth;
-	height = window.innerHeight;
+	width = container.offsetWidth;
+	height = container.offsetHeight;
 
 	ctx.canvas.width = width;
 	ctx.canvas.height = height;
@@ -45,8 +48,7 @@ var drawCircle = function(x,y,radius){
 	ctx.beginPath();
 	ctx.arc(x,y,radius,0,2*Math.PI);
 
-	// ctx.strokeStyle = "black";
-	// ctx.stroke();
+	
 	ctx.shadowBlur=20;
 	ctx.shadowColor="white";
 	ctx.fillStyle = "white";
@@ -54,25 +56,83 @@ var drawCircle = function(x,y,radius){
 
 };
 
+var drawFlake = function(x,y,lineLength, n) {
+	
+	
+	var theta = (2*Math.PI)/n;
+		
+	ctx.strokeStyle = "white";
+	// ctx.shadowBlur=30;
+	ctx.shadowColor="white";
+
+	for (var i =0; i < n; i++) {
+
+		var dx = Math.round((lineLength/2) * Math.cos(theta*i));
+		var dy = Math.round((lineLength/2) * Math.sin(theta*i));
+			
+		ctx.beginPath();
+		ctx.moveTo(x, y);
+		ctx.lineTo((x + dx), (y + dy));
+		ctx.stroke();			
+	}
+		
+
+};
+
+
+var fillBoxAnim = function(boxWidth, boxHeight, percentage){
+	bannerX = (width/2) - (boxWidth/2);
+	bannerY = (height - 60);
+	bannerWidth = boxWidth;
+	bannerHeight = boxHeight;
+	ctx.shadowBlur=20;
+	ctx.shadowColor = "white";
+	ctx.fillStyle = "black";
+	
+	ctx.rect(bannerX, bannerY, bannerWidth, (percentage) * bannerHeight);
+	ctx.fill();
+
+};
+
+
+
+var percent = 0;
 
 
 var render = function() {
 	ctx.clearRect(0,0,width,height);
+	
 	requestAnimationFrame(render); 
 
-  //   var now = new Date().getTime(),
-  //    dt = now - (time || now);
- 	// var time = now;
-    
-    
+   
  	for (var i = 0; i < particles.length; i++) {
  		
- 		if (particles[i].position.y >= height + particles[i].radius) {
+ 		
+		if (particles[i].position.y >= height + particles[i].radius) {
+
  			 particles[i].position.y = 0 - particles[i].radius;
+ 			 if (percent < 100) {
+ 			 	percent += 1;
+ 			 }
+
  		}
- 			drawCircle(particles[i].position.x += particles[i].speed.x , 
- 				   particles[i].position.y += particles[i].speed.y, 
+ 		if (particles[i].position.x >= width + particles[i].radius) {
+
+ 			 particles[i].position.x = 0 - particles[i].radius;
+ 			 if (percent < 100) {
+ 			 	percent += 1;
+ 			 }
+
+ 		}
+			// drawFlake(particles[i].position.x += particles[i].speed.x , 
+ 		// 		  	 particles[i].position.y += particles[i].speed.y, 
+ 		// 		   	particles[i].radius, 6);
+	
+		
+		drawCircle(particles[i].position.x += particles[i].speed.x , 
+			   	   particles[i].position.y += particles[i].speed.y, 
  				   particles[i].radius);
+		
  	}
     
   
@@ -86,14 +146,15 @@ var Particle = function() {
 
 }
 
-var createParticles = function(numParticles) {
+var createParticles = function(numParticles, radius, speed, wind) {
 	
 	for (var i = 0; i < numParticles; i++) {
 		var p = new Particle();
-		p.radius = (Math.random() * 9) + 1;
+		p.radius = (Math.random() * radius) + 1;
 		p.position.x = Math.random() * width;
 		p.position.y = -1 * Math.random()*height;
-		p.speed.y = 2 + (5 * Math.random());
+		p.speed.y = 2 + (speed * Math.random());
+		p.speed.x = (0.5 * wind) - (wind * Math.random());
 
 		particles.push(p);
 	}
